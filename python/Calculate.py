74b7c5d7fb37305463feb725cfccc284bde07bb3
GPN211314 <1557873969@qq.com>
2019-01-15 16:46:12 +0800

    将功能细分

--- python/Calculate.py
+++ python/Calculate.py
@@ -3,6 +3,11 @@
 
 from fractions import Fraction
 
+# 将**替换为^
+def instead(expression):
+    tmp_ls = expression.split("**")
+    return "^".join(tmp_ls)
+
 
 # 定义优先级
 def precedence(x):
@@ -38,6 +43,18 @@ def SYA(i, num_stack, ops_stack):
     else:
         ops_stack.append(i)
 
+# 表达式计算
+def exps_cal(exps_list):
+    # 操作数栈与符号栈
+    num_stack = []
+    ops_stack = []
+    for i in exps_list:
+        if type(i) == int:
+            num_stack.append(Fraction(i))
+        else:
+            SYA(i, num_stack, ops_stack)
+    return num_stack.pop()
+
 
 def calculate(a, b, op):
     if op == "+":
@@ -51,6 +68,39 @@ def calculate(a, b, op):
     if op == "^":
         return a ** b
 
+# 将数字与符号分离
+def separate(expression):
+    exps_list = []
+    for i in expression:
+        if i.isdigit():
+            exps_list[-1] = 10 * exps_list[-1] + int(i)
+        elif i != " ":
+            if len(exps_list) >= 2:
+                if (i == "(" and not exps_list[-2].isdigit()) or \
+                        (not i.isdigit() and exps_list[-2] == ")"):
+                    exps_list[-1] = i
+                    exps_list.append(0)
+                else:
+                    exps_list.append(i)
+                    exps_list.append(0)
+            else:
+                exps_list.append(i)
+                exps_list.append(0)
+    exps_list.pop()
+    return exps_list
+
+# 判断对错并统计
+def tof(standard_answer, count, right_count):
+    answer = input()
+    if answer == "":
+        print("共完成{0}道，正确{1}道".format(count, right_count))
+        exit(0)
+    elif Fraction(answer) == standard_answer:
+        print("正确！")
+        right_count += 1
+    else:
+        print("错误！")
+    return count, right_count
 
 def main():
     count = -1
@@ -63,48 +113,11 @@ def main():
 
             expression = "(" + line + ")"
 
-            exps_list = []
+            expression = instead(expression)
 
-            # 将**替换为^
-            tmp_ls = expression.split("**")
-            expression = "^".join(tmp_ls)
-            # 将数字与符号分离
-            for i in expression:
-                if i.isdigit():
-                    exps_list[-1] = 10 * exps_list[-1] + int(i)
-                elif i != " ":
-                    if len(exps_list) >= 2:
-                        if (i == "(" and not exps_list[-2].isdigit()) or \
-                                (not i.isdigit() and exps_list[-2] == ")"):
-                            exps_list[-1] = i
-                            exps_list.append(0)
-                        else:
-                            exps_list.append(i)
-                            exps_list.append(0)
-                    else:
-                        exps_list.append(i)
-                        exps_list.append(0)
-            exps_list.pop()
+            exps_list = separate(expression)
 
-            # 操作数栈与符号栈
-            num_stack = []
-            ops_stack = []
-
-            for i in exps_list:
-                if type(i) == int:
-                    num_stack.append(Fraction(i))
-                else:
-                    SYA(i, num_stack, ops_stack)
-
-            standard_answer = num_stack.pop()
-            answer = input()
-            if answer == "":
-                print("共完成{0}道，正确{1}道".format(count, right_count))
-                exit(0)
-            elif Fraction(answer) == standard_answer:
-                print("正确！")
-                right_count += 1
-            else:
-                print("错误！")
+            standard_answer = exps_cal(exps_list)
+            count ,right_count = tof(standard_answer, count, right_count)
     print("共完成{0}道，正确{1}道".format(count + 1, right_count))
 
